9fda2243de51b07b299d3f130d086cfa2f62126e
Marcel Fransen <m.fransen@metrological.com>
2023-07-17 16:54:18 +0200

    [PackMan] Renamed Firebolt implementation to ExternalService

--- ExternalServiceImplementation/ExternalServiceImplementation.cpp
+++ ExternalServiceImplementation/ExternalServiceImplementation.cpp
@@ -2,7 +2,7 @@
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
  *
- * Copyright 2023 RDK Management
+ * Copyright 2023 Comcast Cable Communications Management, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,7 +62,7 @@ public:
     }
 
     string Information() const override {
-        return _T("{\"identifier\":\"Firebolt PackageManager service\"}");
+        return _T("{\"identifier\":\"ExternalService PackageManager service\"}");
     }
 
     // IPackageManager implementation
--- Module.h
+++ Module.h
@@ -2,8 +2,8 @@
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
  *
- * Copyright 2023 RDK Management
- *
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
